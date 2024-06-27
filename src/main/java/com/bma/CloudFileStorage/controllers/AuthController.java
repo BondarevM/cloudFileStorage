3193ea998ee790a36ec34671cc55598af6dce01e
Mikhail Bondarev <bondarevmisha99@gmail.com>
2024-06-27 12:54:11 +0700

    Validation implemented

--- src/main/java/com/bma/CloudFileStorage/controllers/AuthController.java
+++ src/main/java/com/bma/CloudFileStorage/controllers/AuthController.java
@@ -1,6 +1,7 @@
 package com.bma.CloudFileStorage.controllers;
 
 import com.bma.CloudFileStorage.models.dto.CustomerDto;
+import com.bma.CloudFileStorage.repositories.CustomerRepository;
 import com.bma.CloudFileStorage.services.RegistrationService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,31 +12,48 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class AuthController extends AbstractController{
+public class AuthController extends AbstractController {
     private final RegistrationService registrationService;
+    private final CustomerRepository customerRepository;
 
     @Autowired
-    public AuthController(RegistrationService registrationService) {
+    public AuthController(RegistrationService registrationService, CustomerRepository customerRepository) {
         this.registrationService = registrationService;
+        this.customerRepository = customerRepository;
     }
 
     @GetMapping("/login")
-    public String login (){
+    public String login() {
         return "/login";
     }
 
     @GetMapping("/registration")
-    public String registration (@ModelAttribute("customerDto") CustomerDto customerDto){
+    public String registration(@ModelAttribute("customerDto") CustomerDto customerDto) {
         return "/registration";
     }
 
     @PostMapping("/registration")
     public String performRegistration(@ModelAttribute("customerDto") @Valid CustomerDto customerDto,
-                                      BindingResult bindingResult){
+                                      BindingResult bindingResult) {
+
+        validateCustomer(customerDto, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "/registration";
+        }
 
         registrationService.register(customerDto);
         return "redirect:/login";
     }
 
+    private void validateCustomer(CustomerDto customerDto,
+                                  BindingResult bindingResult) {
+        if (!customerDto.getPassword().equals(customerDto.getConfirmedPassword())) {
+            bindingResult.rejectValue("confirmedPassword", "400", "Password mismatch");
+        }
 
+        if (customerRepository.findByLogin(customerDto.getLogin()).isPresent()){
+            bindingResult.rejectValue("login", "400", "User with this login already exist");
+        }
+    }
 }
--- src/main/java/com/bma/CloudFileStorage/models/dto/CustomerDto.java
+++ src/main/java/com/bma/CloudFileStorage/models/dto/CustomerDto.java
@@ -1,5 +1,7 @@
 package com.bma.CloudFileStorage.models.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +11,11 @@ public class CustomerDto {
 
     private Integer id;
 
+    @NotEmpty(message = "Login should not be empty")
+    @Size(min = 4, max = 30, message = "Login must consist of at least 4 characters")
     private String login;
 
+    @Size(min = 4, max = 30, message = "Password must consist of at least 4 characters")
     private String password;
 
     private String confirmedPassword;
